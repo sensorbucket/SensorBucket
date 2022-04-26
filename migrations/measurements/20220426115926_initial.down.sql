4506a71c09e4be8f378932f45b214dd0f0f24fb4
Tim van Osch <timvosch@pollex.nl>
2022-04-26 15:15:02 +0200

    Fix migrations, remove PK

--- migrations/measurements/20220426115926_initial.down.sql
+++ migrations/measurements/20220426115926_initial.down.sql
@@ -1,12 +1,1 @@
-CREATE TABLE measurements (
-  thing_urn VARCHAR NOT NULL,
-  timestamp TIMESTAMPTZ NOT NULL,
-  value DOUBLE PRECISION NOT NULL,
-  measurement_type VARCHAR NOT NULL,
-  measurement_type_unit VARCHAR NOT NULL,
-  location_id INTEGER,
-  coordinates GEOGRAPHY(POINT,4326) NOT NULL,
-  metadata JSONB,
-  
-  PRIMARY KEY (thing_urn, timestamp)
-);
+DROP TABLE measurements;
--- migrations/measurements/20220426115926_initial.up.sql
+++ migrations/measurements/20220426115926_initial.up.sql
@@ -1,1 +1,13 @@
-DROP TABLE measurements;
+CREATE EXTENSION IF NOT EXISTS "postgis";
+
+CREATE TABLE measurements (
+  thing_urn VARCHAR NOT NULL,
+  timestamp TIMESTAMPTZ NOT NULL,
+  value DOUBLE PRECISION NOT NULL,
+  measurement_type VARCHAR NOT NULL,
+  measurement_type_unit VARCHAR NOT NULL,
+  location_id INTEGER,
+  coordinates GEOGRAPHY(POINT,4326) NOT NULL,
+  metadata JSONB
+);
+SELECT create_hypertable('measurements', 'timestamp');
