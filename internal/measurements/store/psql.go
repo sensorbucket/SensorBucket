01fea29b24b98370002bab2e8857abf4d1bfab71
Tim van Osch <timvanosch@senwize.com>
2022-05-24 22:44:15 +0200

    Chore(measurements): Remove db model Chore(measurements): Remove unnecessary if at insert

--- internal/measurements/store/psql.go
+++ internal/measurements/store/psql.go
@@ -27,28 +27,22 @@ func NewPSQL(db *sqlx.DB) *MeasurementStorePSQL {
 }
 
 func (s *MeasurementStorePSQL) Insert(m measurements.Measurement) error {
-	model := toModel(m)
-
 	values := map[string]interface{}{
-		"thing_urn":             model.ThingURN,
-		"timestamp":             model.Timestamp,
-		"value":                 model.Value,
-		"measurement_type":      model.MeasurementType,
-		"measurement_type_unit": model.MeasurementTypeUnit,
-		"metadata":              model.Metadata,
+		"thing_urn":             m.ThingURN,
+		"timestamp":             m.Timestamp,
+		"value":                 m.Value,
+		"measurement_type":      m.MeasurementType,
+		"measurement_type_unit": m.MeasurementTypeUnit,
+		"metadata":              m.Metadata,
+		"location_id":           m.LocationID,
+		"location_name":         m.LocationName,
 	}
 
-	if model.LocationID.Valid {
-		values["location_id"] = model.LocationID.Int64
+	if m.LocationLongitude != nil && m.LocationLatitude != nil {
+		values["location_coordinates"] = sq.Expr("ST_SETSRID(ST_POINT(?,?),4326)", m.LocationLongitude, m.LocationLatitude)
 	}
-	if model.LocationName.Valid {
-		values["location_name"] = model.LocationName.String
-	}
-	if model.LocationLongitude.Valid && model.LocationLatitude.Valid {
-		values["location_coordinates"] = sq.Expr("ST_SETSRID(ST_POINT(?,?),4326)", model.LocationLongitude.Float64, model.LocationLatitude.Float64)
-	}
-	if model.Longitude.Valid && model.Latitude.Valid {
-		values["coordinates"] = sq.Expr("ST_SETSRID(ST_POINT(?,?),4326)", model.Longitude.Float64, model.Latitude.Float64)
+	if m.Longitude != nil && m.Latitude != nil {
+		values["coordinates"] = sq.Expr("ST_SETSRID(ST_POINT(?,?),4326)", m.Longitude, m.Latitude)
 	}
 
 	query, params, err := pq.Insert("measurements").SetMap(values).ToSql()
@@ -118,7 +112,7 @@ func (s *MeasurementStorePSQL) Query(query measurements.Query, p measurements.Pa
 	list := make([]measurements.Measurement, 0, p.Limit)
 	var nextPage *measurements.Pagination
 	for rows.Next() {
-		var m model
+		var m measurements.Measurement
 		err = rows.Scan(
 			&m.ThingURN,
 			&m.Timestamp,
@@ -138,7 +132,7 @@ func (s *MeasurementStorePSQL) Query(query measurements.Query, p measurements.Pa
 		}
 
 		if len(list) < p.Limit {
-			list = append(list, m.toValue())
+			list = append(list, m)
 		} else {
 			ts := m.Timestamp.Unix()
 			nextPage = &measurements.Pagination{
