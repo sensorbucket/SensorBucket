675c3ec5e80fef2f4d5c7bce70d517fefd599c39
Tim van Osch <timvanosch@senwize.com>
2022-09-18 11:38:54 +0200

    Add web and env pkg

---
+++ internal/env/env.go
@@ -0,0 +1,21 @@
+package env
+
+import (
+	"fmt"
+	"os"
+)
+
+func Could(key, value string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		v = value
+	}
+	return v
+}
+func Must(key string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		panic(fmt.Sprintf("environment variable (%s) must be set", key))
+	}
+	return v
+}
---
+++ internal/web/web.go
@@ -0,0 +1,91 @@
+package web
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// APIResponse ...
+type APIResponse struct {
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+// APIError is an API consumer friendly error
+type APIError struct {
+	Message    string `json:"message,omitempty"`
+	Code       string `json:"code,omitempty"`
+	HTTPStatus int    `json:"-"`
+}
+
+func (e *APIError) Error() string {
+	return e.Message
+}
+
+func NewError(status int, message string, code string) error {
+	return &APIError{
+		HTTPStatus: status,
+		Message:    message,
+		Code:       code,
+	}
+}
+
+var (
+	// ContentTypeError ...
+	ContentTypeError = &APIError{
+		HTTPStatus: 400,
+		Message:    "Invalid content type",
+		Code:       "INVALID_CONTENT_TYPE",
+	}
+	// InvalidJSONError ...
+	InvalidJSONError = &APIError{
+		HTTPStatus: 400,
+		Message:    "Malformed JSON",
+		Code:       "MALFORMED_JSON",
+	}
+)
+
+// DecodeJSON ...
+func DecodeJSON(r *http.Request, v interface{}) error {
+	if r.Header.Get("content-type") != "application/json" {
+		return ContentTypeError
+	}
+
+	err := json.NewDecoder(r.Body).Decode(v)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error decoding JSON: %s\n", err)
+		return InvalidJSONError
+	}
+	return nil
+}
+
+// HTTPResponse ...
+func HTTPResponse(w http.ResponseWriter, s int, r interface{}) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(s)
+	json.NewEncoder(w).Encode(r)
+}
+
+// HTTPError writes the error to a http response writer
+func HTTPError(w http.ResponseWriter, err error) {
+	w.Header().Set("content-type", "application/json")
+
+	var apierror *APIError
+	if errors.As(err, &apierror) {
+		w.WriteHeader(apierror.HTTPStatus)
+		json.NewEncoder(w).Encode(&APIError{
+			Message: err.Error(),
+			Code:    apierror.Code,
+		})
+		return
+	}
+
+	fmt.Printf("non APIError occured: %s\n", err)
+	w.WriteHeader(500)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "Internal server error",
+	})
+}
