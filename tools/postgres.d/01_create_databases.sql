1ba4eecb9e8eb563a4fec9a2fac80d4045d2b4bf
Tim van Osch <timvanosch@pollex.nl>
2023-07-07 16:18:42 +0200

    Chore

--- tools/postgres.d/01_create_databases.sql
+++ tools/postgres.d/01_create_databases.sql
@@ -1,19 +1,6 @@
--- Create Device Service database
-CREATE DATABASE deviceservice;
-CREATE USER deviceservice WITH ENCRYPTED PASSWORD 'deviceservice';
-GRANT ALL PRIVILEGES ON DATABASE deviceservice TO deviceservice;
-\c deviceservice
+CREATE DATABASE sensorbucket;
+CREATE USER sensorbucket WITH ENCRYPTED PASSWORD 'sensorbucket';
+GRANT ALL PRIVILEGES ON DATABASE sensorbucket TO sensorbucket;
+\c sensorbucket
 CREATE EXTENSION postgis;
-
--- Create Measurements Service database
-CREATE DATABASE measurementservice;
-CREATE USER measurementservice WITH ENCRYPTED PASSWORD 'measurementservice';
-GRANT ALL PRIVILEGES ON DATABASE measurementservice TO measurementservice;
-\c measurementservice
 CREATE EXTENSION timescaledb;
-CREATE EXTENSION postgis;
-
--- Create Pipeline Service database
-CREATE DATABASE pipelineservice;
-CREATE USER pipelineservice WITH ENCRYPTED PASSWORD 'pipelineservice';
-GRANT ALL PRIVILEGES ON DATABASE pipelineservice TO pipelineservice;
