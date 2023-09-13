65ac208cdef96d90ab08ba6d4a2f761ef5c5f880
Tim van Osch <timvanosch@pollex.nl>
2023-09-13 09:51:31 +0200

    Add latest measurement to datastream detail endpoint

--- services/core/transport/http.go
+++ services/core/transport/http.go
@@ -2,10 +2,12 @@ package coretransport
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 
+	"sensorbucket.nl/sensorbucket/internal/pagination"
 	"sensorbucket.nl/sensorbucket/internal/web"
 	"sensorbucket.nl/sensorbucket/services/core/devices"
 	"sensorbucket.nl/sensorbucket/services/core/measurements"
@@ -18,8 +20,10 @@ func Create(r chi.Router, measurementService *measurements.Service, deviceServic
 
 type GetDatastreamResponse struct {
 	*measurements.Datastream
-	Device *devices.Device
-	Sensor *devices.Sensor
+	Device               *devices.Device `json:"device"`
+	Sensor               *devices.Sensor `json:"sensor"`
+	MeasurementValue     float64         `json:"measurement_value"`
+	MeasurementTimestamp time.Time       `json:"measurement_timestamp"`
 }
 
 func getDatastreams(measurementService *measurements.Service, deviceService *devices.Service) http.HandlerFunc {
@@ -48,14 +52,23 @@ func getDatastreams(measurementService *measurements.Service, deviceService *dev
 			web.HTTPError(w, err)
 			return
 		}
+		res := GetDatastreamResponse{
+			Datastream: ds,
+			Device:     device,
+			Sensor:     sensor,
+		}
+
+		m, err := measurementService.QueryMeasurements(measurements.Filter{
+			Datastream: []string{ds.ID.String()},
+		}, pagination.Request{Limit: 1})
+		if len(m.Data) > 0 {
+			res.MeasurementValue = m.Data[0].MeasurementValue
+			res.MeasurementTimestamp = m.Data[0].MeasurementTimestamp
+		}
 
 		web.HTTPResponse(w, http.StatusOK, web.APIResponseAny{
 			Message: "",
-			Data: GetDatastreamResponse{
-				Datastream: ds,
-				Device:     device,
-				Sensor:     sensor,
-			},
+			Data:    res,
 		})
 	}
 }
