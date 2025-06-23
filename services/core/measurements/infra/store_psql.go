5b952d47c66aa5c1d180a45315e11ccc3546d132
Tim van Osch <timvanosch@pollex.nl>
2025-06-23 18:47:23 +0200

    fix(core): add missing pgx import

--- services/core/measurements/infra/store_psql.go
+++ services/core/measurements/infra/store_psql.go
@@ -9,6 +9,7 @@ import (
 
 	sq "github.com/Masterminds/squirrel"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"sensorbucket.nl/sensorbucket/internal/pagination"
